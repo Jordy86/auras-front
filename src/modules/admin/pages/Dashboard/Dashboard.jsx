d842450358ad698a852024636026d95ee0266eae
MaheninaRan <120193477+MaheninaRan@users.noreply.github.com>
2025-01-28 13:46:59 +0300

    start dashboard

--- src/modules/admin/pages/Dashboard/Dashboard.jsx
+++ src/modules/admin/pages/Dashboard/Dashboard.jsx
@@ -1,22 +1,55 @@
 import { Link } from "react-router-dom";
 import AdminMenu from "../../component/MenuAdmin/AdminMenu";
 import './Dashboard.css';
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
+const Dashboard = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(2025); 
+  const [revenue, setRevenue] = useState(50000000); 
 
-const Dashboard = () =>{
-    const [menuOpen, setMenuOpen] = useState(false);
-    const toggleMenu = () => setMenuOpen(!menuOpen);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
+  const years = Array.from({ length: 6 }, (_, i) => 2020 + i);
 
-    return (
-        <>
-            <AdminMenu menuOpen={menuOpen} toggleMenu={toggleMenu}/>
-            <div className={`dashboard-content ${menuOpen ? "menu-open" : ""}`}>
-                <h1>Coucou Dashboard</h1>
-                <p>Bienvenue dans votre espace d'administration !</p>
+  const handleYearChange = (event) => {
+    const year = event.target.value;
+    setSelectedYear(year);
+
+    if (year === "2020") {
+      setRevenue(50000000); 
+    } else if (year === "2025") {
+      setRevenue(60000000); 
+    } else {
+      setRevenue(40000000); 
+    }
+  };
+
+  return (
+    <>
+      <AdminMenu menuOpen={menuOpen} toggleMenu={toggleMenu} />
+      <div className={`dashboard-content ${menuOpen ? "menu-open" : ""}`}>
+        <div className="row">
+            <div className="card">
+              <h3>Chiffre d'affaire pour l'année {selectedYear} 
+                  <select id="year" value={selectedYear} onChange={handleYearChange}>
+                  {years.map((year) => (
+                      <option key={year} value={year}>
+                      {year}
+                      </option>
+                  ))}
+                  </select>
+              </h3>
+              <p>{revenue.toLocaleString()} Ariary</p> 
             </div>
-        </>
-    );
-}
+            <div className="card-rigth">
+                  Zavatra
+            </div>
+        </div>
+        
+      </div>
+    </>
+  );
+};
+
 export default Dashboard;
